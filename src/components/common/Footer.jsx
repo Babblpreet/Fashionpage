26285acb1225c5821517f00b99c5b6141aa29b9c
Babblpreet <babblpreet605@gmail.com>
2025-02-09 15:28:34 +0530

    n

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,72 +1,81 @@
-import React, { useState } from 'react';
+import React from 'react'
+import shopmap from '../../components/json/footershopping.json'
+import helpmap from '../../components/json/footerhelp.json'
+import companymap from '../../components/json/footercompany.json'
+import popularmap from '../../components/json/footerpopular.json'
+import contactmap from '../../components/json/footercontact.json'
+
 
 const Footer = () => {
-  const [currency, setCurrency] = useState(false);
-  const [selectedCurrency, setSelectedCurrency] = useState("USD");
-
-  // SVG Icons for each currency
-  const currencyIcons = {
-    USD: (
-      <svg fill="#000000" width="16px" height="16px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M14,11H10a2,2,0,0,1,0-4h5a1,1,0,0,1,1,1,1,1,0,0,0,2,0,3,3,0,0,0-3-3H13V3a1,1,0,0,0-2,0V5H10a4,4,0,0,0,0,8h4a2,2,0,0,1,0,4H9a1,1,0,0,1-1-1,1,1,0,0,0-2,0,3,3,0,0,0,3,3h2v2a1,1,0,0,0,2,0V19h1a4,4,0,0,0,0-8Z" />
-      </svg>
-    ),
-    RUB: (
-      <svg fill="#000000" width="16px" height="16px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M14,12A5,5,0,0,0,14,2H9A1,1,0,0,0,8,3V14H6a1,1,0,0,0,0,2H8v5a1,1,0,0,0,2,0V16h5a1,1,0,0,0,0-2H10V12ZM10,4h4a3,3,0,0,1,0,6H10Z" />
-      </svg>
-    ),
-    EUR: (
-      <svg width="16px" height="16px" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M13.374 3C12.2848 1.48593 10.5075 0.5 8.5 0.5C5.18629 0.5 2.5 3.18629 2.5 6.5V8.5C2.5 11.8137 5.18629 14.5 8.5 14.5C10.5075 14.5 12.2848 13.5141 13.374 12M0 5.5H7M0 9.5H7" stroke="#000000" />
-      </svg>
-    ),
-    INR: (
-      <svg width="16px" height="16px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M7 13h3.75c1.393 0 2.728-.474 3.713-1.318C15.447 10.838 16 9.693 16 8.5c0-1.193-.553-2.338-1.537-3.182C13.478 4.474 12.143 4 10.75 4H7M7 13l8.2 7M7 8.5h11M11 4h7" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" />
-      </svg>
-    ),
-  };
-
-  const handleCurrency = (selected) => {
-    setSelectedCurrency(selected);
-    setCurrency(false);
-  };
-
   return (
     <>
-      <div className="flex items-center gap-2">
-        <div className="relative inline-block">
-          {/* Dropdown Button */}
-          <button
-            onClick={() => setCurrency(!currency)}
-            className="flex items-center py-2 px-3 border rounded shadow-sm"
-          >
-            {currencyIcons[selectedCurrency]}
-            <h2 className="font-mulish_regular text-xs leading-5 text-gray-600 pl-2">
-              {selectedCurrency}
-            </h2>
-          </button>
-
-          {/* Dropdown Menu */}
-          {currency && (
-            <div className="absolute top-full left-0 mt-2 bg-white border border-gray-200 shadow-md rounded-md w-32 z-10">
-              {Object.entries(currencyIcons).map(([key, icon]) => (
-                <div
-                  key={key}
-                  className="flex items-center py-2 px-4 text-sm hover:bg-gray-100 cursor-pointer"
-                  onClick={() => handleCurrency(key)}
-                >
-                  {icon}
-                  <span className="pl-2">{key}</span>
+      <footer className='bg-[#263238]'>
+        <div className='padcontain'>
+          <div className='container'>
+            <div className='flex gap-[30px]'>
+              <div className='max-w-[240px] w-full'>
+                <h2 className='mb-5 text-white font-mulish_medium text-xl leading-[30px]'>Shopping</h2>
+                <div className='flex flex-col gap-[14px]'>
+                  {
+                    shopmap.map((item, index) => (
+                      <h2 key={index} className='py-[3px] text-[#CFD8DC] font-mulish_regular text-base leading-[25px] cursor-pointer'>{item.name}</h2>
+                    ))
+                  }
                 </div>
-              ))}
+              </div>
+              <div className='max-w-[240px] w-full'>
+                <h2 className='mb-5 text-white font-mulish_medium text-xl leading-[30px]'>Help Desk</h2>
+                <div className='flex flex-col gap-[14px]'>
+                  {
+                    helpmap.map((item, index) => (
+                      <h2 key={index} className='py-[3px] text-[#CFD8DC] font-mulish_regular text-base leading-[25px] cursor-pointer'>{item.name}</h2>
+                    ))
+                  }
+                </div>
+              </div>
+              <div className='max-w-[240px] w-full'>
+                <h2 className='mb-5 text-white font-mulish_medium text-xl leading-[30px]'>Company</h2>
+                <div className='flex flex-col gap-[14px]'>
+                  {
+                    companymap.map((item, index) => (
+                      <h2 key={index} className='py-[3px] text-[#CFD8DC] font-mulish_regular text-base leading-[25px] cursor-pointer'>{item.name}</h2>
+                    ))
+                  }
+                </div>
+              </div>
+              <div className='max-w-[375px] w-full'>
+                <h2 className='mb-5 text-white font-mulish_medium text-xl leading-[30px]'>Popular Searches</h2>
+                <div className="grid grid-cols-2 gap-y-[14px] gap-x-[10px]">
+                  {popularmap.map((item, index) => (
+                    <h2 key={index} className="py-[3px] text-[#CFD8DC] font-mulish_regular text-base leading-[25px] cursor-pointer" >  {item.name}
+                    </h2>
+                  ))}
+                </div>
+              </div>
+              <div className='max-w-[375px] w-full'>
+                <h2 className='mb-5 text-white font-mulish_medium text-xl leading-[30px]'>Contact Us</h2>
+                <div className='flex flex-col gap-[17px]'>
+                  {
+                    contactmap.map((item, index) => (
+                      <div key={index} className='text-[#CFD8DC] font-mulish_regular flex flex-col gap-1'>
+                        <div className='flex gap-2'>
+                          <img className='py-[3px]' src={item.image} alt={item.name} />
+                          <h2 className='text-sm leading-[22px]'>{item.name}</h2>
+                        </div>
+                        <h2 className='text-base leading-[25px] max-w-[262px] w-full'>{item.title}</h2>
+                      </div>
+                    ))
+                  }
+                </div>
+              </div>
             </div>
-          )}
+            <span className='mt-[60px] mb-[30px] w-full max-w-full border border-gray-lightgray block'></span>
+            <div></div>
+          </div>
         </div>
-      </div>
+      </footer >
     </>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
